f2071b03fa41a2f7eff8491224237ced07e898d7
zohidjon-m <148512253+zohidjon-m@users.noreply.github.com>
2024-11-02 09:29:51 +0900

    finished the stopwatch

--- Stopwatch.java
+++ Stopwatch.java
@@ -11,7 +11,7 @@ public class Stopwatch implements ActionListener {
     JButton pauseButton = new JButton("PAUSE");
     JButton resetButton = new JButton("RESET");
     JLabel timeLabel = new JLabel();
-    int elapsedTime = 0;
+    int elapsedTime = 60000;
     int seconds = 0;
     int minutes = 0;
     int hours = 0;
@@ -19,6 +19,20 @@ public class Stopwatch implements ActionListener {
     String seconds_string = String.format("%02d", seconds);
     String minutes_string = String.format("%02d", minutes);
     String hours_string = String.format("%02d", hours);
+
+    Timer timer = new Timer(1000, new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            elapsedTime+= 1000;
+            hours = (elapsedTime/3600000);
+            minutes = (elapsedTime/60000)%60;
+            seconds = (elapsedTime/1000)%60;
+            seconds_string = String.format("%02d", seconds);
+            minutes_string = String.format("%02d", minutes);
+            hours_string = String.format("%02d", hours);
+            timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+
+        }
+    });
 
 
 
@@ -54,13 +68,45 @@ public class Stopwatch implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == startButton){
 
+            if(started == false){
+                started = true;
+                startButton.setText("STOP");
+                start();
+            }
+            else{
+                started = false;
+                startButton.setText("START");
+                stop();
+            }
+        }
+
+        if(e.getSource() == resetButton){
+            started = false;
+            startButton.setText("START");
+            reset();
+        }
     }
 
     void start(){
-
+        timer.start();
     }
     void stop(){
+        timer.stop();
+    }
+
+    void reset(){
+        timer.stop();
+        elapsedTime = 0;
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        seconds_string = String.format("%02d", seconds);
+        minutes_string = String.format("%02d", minutes);
+        hours_string = String.format("%02d", hours);
+        timeLabel.setText(hours_string + ":" + minutes_string + ":" + seconds_string);
+
 
     }
 
